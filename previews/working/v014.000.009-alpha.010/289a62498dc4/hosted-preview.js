13e0c591e7627319f9221ea81231dbe4773c3233
paravane-dev <security@paravane.io>
2026-09-08 17:26:01 -0400

    Migrate working previews to tagged archive

--- previews/working/v014.000.009-alpha.010/289a62498dc4/hosted-preview.js
+++
@@ -1,7 +0,0 @@
-(function () {
-  "use strict";
-  document.addEventListener("submit", function (event) {
-    event.preventDefault();
-    window.alert("This archived preview is view-only. Forms are disabled.");
-  }, true);
-})();
